a07b62eeba637c387ce64bfae6c08b2a2dddf55c
Gubina Irina <irina.gubina98@mail.ru>
2020-11-27 21:05:17 +0300

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnPostInquiry() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
